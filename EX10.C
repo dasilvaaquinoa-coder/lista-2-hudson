0859fe46923b8ff31fbf2e2c76b6bd960454b447
dasilvaaquinoa-coder <dasilvaaquinoa@gmail.com>
2026-08-28 17:17:47 -0300

    Add files via upload

---
+++ EX10.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (){
+
+    int  tabuada 1 , tabuada 2 , tabuada 3 , tabuada 10; 
+
+    printf ("Digite a tabuada que voce deseja:");
+    scanf ("%d", &tabuada 1);
+    printf ("Digite a tabuada que voce deseja:");
+    scanf ("%d", &tabuada 2);
+    printf ("Digite a tabuada que voce deseja:");
+    scanf ("%d" , &tabuada 3);
+    printf ("Digite a tabuada que voce deseja:");
+    scanf ("%d" , &tabuada 10);
+
+    for (int i = 1; i <= 10; i++){
+        printf ("%d x %d = %d\n", tabuada 1, i, tabuada 1 * i);
+        printf ("%d x %d = %d\n", tabuada 2, i, tabuada 2 * i);
+        printf ("%d x %d = %d\n", tabuada 3, i, tabuada 3 * i);
+        printf ("%d x %d = %d\n", tabuada 10, i, tabuada 10 * i);
+
+    }
+     return 0;
+      
+} 
+
+
